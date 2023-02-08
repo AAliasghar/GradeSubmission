4f522bd18f49a48505e281f0ee815be7b45f8a89
AAliasghar <a.asgharinia@yahoo.com>
2023-02-08 23:56:20 +0100

    student update

--- src/main/java/com/ali/gradesubmission/repository/StudentRepository.java
+++ src/main/java/com/ali/gradesubmission/repository/StudentRepository.java
@@ -1,1 +1,7 @@
 package com.ali.gradesubmission.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ali.gradesubmission.entity.Student;
+
+public interface StudentRepository extends CrudRepository<Student, Long>{}
--- src/main/java/com/ali/gradesubmission/web/StudentController.java
+++ src/main/java/com/ali/gradesubmission/web/StudentController.java
@@ -2,6 +2,7 @@ package com.ali.gradesubmission.web;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,20 +14,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ali.gradesubmission.entity.Student;
+import com.ali.gradesubmission.service.StudentService;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
 
-    
+    @Autowired StudentService studentService;
+
     @GetMapping("/{id}")
     public ResponseEntity<Student> getStudent(@PathVariable Long id) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
-        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student) {    // @RequestBody deserialize JSON properties into Student object
+        return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
