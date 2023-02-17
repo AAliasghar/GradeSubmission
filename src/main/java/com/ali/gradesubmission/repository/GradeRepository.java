260d90363da6f540f3119ac027a6ade9f5f2934b
AAliasghar <a.asgharinia@yahoo.com>
2023-02-17 08:12:32 +0100

    Grade update

--- src/main/java/com/ali/gradesubmission/repository/GradeRepository.java
+++ src/main/java/com/ali/gradesubmission/repository/GradeRepository.java
@@ -1,9 +1,13 @@
 package com.ali.gradesubmission.repository;
 
+import java.util.List;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 
 import com.ali.gradesubmission.entity.Grade;
 
@@ -11,6 +15,9 @@ import com.ali.gradesubmission.entity.Grade;
 public interface GradeRepository extends CrudRepository<Grade, Long> {
     Optional<Grade> findByStudentIdAndCourseId(Long studentId, Long courseId); // Custom query by studentId and
                                                                                // courseId, Optional to check Null
-
+@Transactional
     void deleteByStudentIdAndCourseId(Long studentId, Long courseId);
+    List<Grade> findByStudentId(Long studentId);
+    
+    List<Grade> findByCourseId(Long studentId);
 }
