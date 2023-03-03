50219c34bcf8b6830856ea4871b8568297ead22c
AAliasghar <a.asgharinia@yahoo.com>
2023-03-03 08:32:58 +0100

    service Update

---
+++ Frontend/angular-frontend/src/app/services/grade.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Grade } from '../common/grade';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GradeService {
+
+
+  private baseURL = "http://localhost:8090/api/v1/grades";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getEmployeesList(): Observable<Grade[]> {
+    return this.httpClient.get<Grade[]>(`${this.baseURL}`);
+  }
+}
